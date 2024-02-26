16cd6ead8f8cad5b36d7b19ea4e5f917f5380b7a
ThanhPhuongTran <thanhphuong2000na@gmail.com>
2024-02-26 09:46:40 +0700

    first commit 5

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template
 app=Flask(__name__,template_folder='template')
 @app.route('/')
+@app.route('/home')
 def home():
-    return "Hello Anh Ngoc"
+    return render_template('index.html')
 if __name__=='__main__':
     app.run(debug=True)
