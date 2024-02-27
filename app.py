073f25f46c0e7db76c215925bcc4864c611b98d0
ThanhPhuongTran <thanhphuong2000na@gmail.com>
2024-02-27 14:15:09 +0700

    first commit 5

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask,render_template
-app=Flask(__name__,template_folder='template')
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__, template_folder='template')
+
+def get_static_ip():
+    hostname = socket.gethostname()
+    static_ip = socket.gethostbyname(hostname)
+    return static_ip
+
+def save_static_ip(ip_address):
+    with open('static_ip.txt', 'w') as file:
+        file.write(ip_address)
+
+static_ip = get_static_ip()
+save_static_ip(static_ip)
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
-if __name__=='__main__':
+    return render_template('index.html', static_ip=static_ip)
+
+if __name__ == '__main__':
     app.run(debug=True)
